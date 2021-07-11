8168327c7e27b0ee2977c3291ca3ee8d40ac15de
mohamed hosny <hamadaengineer90@outlook.com>
2021-07-11 15:29:19 +0200

    implements repos

---
+++ CleanArchDemoo/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.RepoInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourceRepository _courseRepository;
+
+        public CourseService(ICourceRepository courceRepository)
+        {
+            _courseRepository = courceRepository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
